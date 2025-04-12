670f359435f6bb45e8aba8fc5a14c2100d18c84a
Oles Odynets <olchyk98@gmail.com>
2025-04-12 21:54:00 +0200

    properly learn child workspace packages
    previously, globMatch would return relative paths
    instead of absolute, which would mess up symlinks stored
    in oink config folder. this led to learned workspace
    packages not being picked up during "root" command call.
    this commit fixes the issue by applying path.resolve
    to the result of globSync which is used in globMatch.
    This commit also adds docs explaining this behaviour.

--- src/utils/glob-match.ts
+++ src/utils/glob-match.ts
@@ -1,5 +1,16 @@
-import { GlobOptions, globSync } from 'glob'
+import path from 'path'
+import process from 'process'
+import { globSync } from 'glob'
 
-export function globMatch (pattern: string | string[], opts: Pick<GlobOptions, 'cwd'>): string[] {
-  return globSync(pattern, opts)
+/**
+ * The function takes single or list of patterns
+ * and traverses from the current directory (can be changed through opts)
+ * to find all files matching the specified patterns.
+ *
+ * Under the hood the function uses 'glob'.globSync, but also
+ * applies path.resolve to always return absolute paths.
+ * */
+export function globMatch (pattern: string | string[], opts: { cwd?: string } = {}): string[] {
+  const cwd = opts.cwd ?? process.cwd()
+  return globSync(pattern, { cwd }).map((item) => path.resolve(cwd, item))
 }
