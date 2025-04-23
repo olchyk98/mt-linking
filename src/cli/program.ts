29c390c9461cfeebc759b699b54180cf164bc77a
Oles Odynets <olchyk98@gmail.com>
2025-04-23 12:58:31 +0200

    0.0.4

--- src/cli/program.ts
+++ src/cli/program.ts
@@ -8,7 +8,7 @@ program
   .description(
     'This is the right tool for linking. Reduce pain - increase productivity.',
   )
-  .version('0.0.3')
+  .version('0.0.4')
 
 export function initProgram () {
   program.parse(process.argv)
