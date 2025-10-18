10891ecbef4856f21fc95ce5daf0532b74311b43
Oles Odynets <olchyk98@gmail.com>
2025-10-18 13:50:54 +0200

    chore: reset core tests
    This commit resets all existing unit test cases for the "core"
    submodule. Previously, the tests were ran while heavily relying
    on stubbing and mocking. This has proven to slow down development
    and NOT properly ensure that the application is bug-free. This
    commit is the first step for the tests reworking with "memfs".

--- test/unit/core/apply-transpilation-result.test.ts
+++ test/unit/core/apply-transpilation-result.test.ts
@@ -1,232 +1,39 @@
-import { afterEach, beforeEach, describe, it, vi } from 'vitest'
-import path from 'path'
-import fs from 'fs'
-import * as GetWorkspaceRootPathForPackageModule from '../../../src/core/get-workspace-root-path-for-package'
-import * as GetPackageAtPathModule from '../../../src/core/get-package-at-path'
-import * as ExecuteShellModule from '../../../src/utils/execute-shell'
-import { applyTranspilationResult } from '../../../src/core/apply-transpilation-result'
-import * as GetLinkingStrategyForPackageModule from '../../../src/core/get-linking-strategy-for-package'
+import { describe, it } from 'vitest'
 
-// XXX: Run this suite sequentially, because
-// we're heavily overriding implementation of functions
-// used between the test cases.
 describe.sequential('applyTranspilationResult', () => {
-  beforeEach(() => {
-    vi.spyOn(ExecuteShellModule, 'executeShell')
-      .mockImplementation(async (_command, _args, opts) => (
-        [ `${path.resolve(opts?.cwd as string ?? '')}/node_modules/source/` ].join('\n')
-      ))
-    vi.spyOn(GetWorkspaceRootPathForPackageModule, 'getWorkspaceRootPathForPackage')
-      .mockReturnValue(null)
+  it('should return null if there is no package at specified source package path', async () => {
+
   })
 
-  afterEach(() => {
-    vi.clearAllMocks()
+  it('should return null if there is no package at specified dest package path', async () => {
+
   })
 
-  it('should return null if there is no package at specified source package path', async ({ expect }) => {
-    vi.spyOn(GetPackageAtPathModule, 'getPackageAtPath')
-      .mockImplementation((absolutePath) => {
-        if (absolutePath === 'dest_path') {
-          return { absolutePath: 'dest_path', packageJson: { name: 'dest' } }
-        }
-        return null
-      })
-    const result = await applyTranspilationResult('source_path', 'dest_path', 'TRANSPILED')
-    expect(result).toEqual(null)
+  it('should return null if linking strategy cannot be identified for source package', async () => {
+
   })
 
-  it('should return null if there is no package at specified dest package path', async ({ expect }) => {
-    vi.spyOn(GetPackageAtPathModule, 'getPackageAtPath')
-      .mockImplementation((absolutePath) => {
-        if (absolutePath === 'source_path') {
-          return { absolutePath: 'source_path', packageJson: { name: 'dest' } }
-        }
-        return null
-      })
-    const result = await applyTranspilationResult('source_path', 'dest_path', 'TRANSPILED')
-    expect(result).toEqual(null)
+  it('should properly apply transpilation result for strategy: TRANSPILED', async () => {
+
   })
 
-  it('should return null if linking strategy cannot be identified for source package', async ({ expect }) => {
-    vi.spyOn(GetLinkingStrategyForPackageModule, 'getLinkingStrategyForPackage')
-      .mockReturnValueOnce(null)
-    const result = await applyTranspilationResult(
-      { absolutePath: 'source_path', packageJson: { name: 'source' } },
-      { absolutePath: 'dest_path', packageJson: { name: 'dest' } },
-    )
-    expect(result).toEqual(null)
+  it('should properly apply transpilation result for workspace package (strategy: TRANSPILED)', async () => {
+
   })
 
-  it('should properly apply transpilation result for strategy: TRANSPILED', async ({ expect }) => {
-    const rmSyncSpy = vi.spyOn(fs, 'rmSync').mockReturnValueOnce(void 0)
-    const cpSyncSpy = vi.spyOn(fs, 'cpSync').mockReturnValueOnce(void 0)
-    const result = await applyTranspilationResult(
-      { absolutePath: 'source_path', packageJson: { name: 'source' } },
-      { absolutePath: 'dest_path', packageJson: { name: 'dest' } },
-      'TRANSPILED',
-    )
-    expect(result).toEqual(true)
-    expect(rmSyncSpy.mock.lastCall).toEqual([
-      path.resolve('dest_path', 'node_modules/source/dist'),
-      { recursive: true },
-    ])
-    expect(cpSyncSpy.mock.lastCall).toEqual([
-      path.resolve('source_path', 'dist'),
-      path.resolve('dest_path', 'node_modules/source/dist'),
-      { recursive: true },
-    ])
+  it('should properly apply transpilation result for strategy: TRANSPILED_LEGACY', async () => {
+
   })
 
-  it('should properly apply transpilation result for workspace package (strategy: TRANSPILED)', async ({ expect }) => {
-    const rmSyncSpy = vi.spyOn(fs, 'rmSync').mockReturnValue(void 0)
-    const cpSyncSpy = vi.spyOn(fs, 'cpSync').mockReturnValue(void 0)
-    vi.spyOn(GetWorkspaceRootPathForPackageModule, 'getWorkspaceRootPathForPackage')
-      .mockReturnValue('workspace_root_path')
-    const result = await applyTranspilationResult(
-      { absolutePath: 'source_path', packageJson: { name: 'source' } },
-      { absolutePath: 'dest_path', packageJson: { name: 'dest' } },
-      'TRANSPILED',
-    )
-    expect(result).toEqual(true)
-    expect(rmSyncSpy.mock.calls).toEqual([
-      [
-        path.resolve('workspace_root_path', 'node_modules/source/dist'),
-        { recursive: true },
-      ],
-      [
-        path.resolve('dest_path', 'node_modules/source/dist'),
-        { recursive: true },
-      ],
-    ])
-    expect(cpSyncSpy.mock.calls).toEqual([
-      [
-        path.resolve('source_path', 'dist'),
-        path.resolve('workspace_root_path', 'node_modules/source/dist'),
-        { recursive: true },
-      ],
-      [
-        path.resolve('source_path', 'dist'),
-        path.resolve('dest_path', 'node_modules/source/dist'),
-        { recursive: true },
-      ],
-    ])
+  it('should properly apply transpilation result for strategy: MAKEFILE_BUILD', async () => {
+
   })
 
-  it('should properly apply transpilation result for strategy: TRANSPILED_LEGACY', async ({ expect }) => {
-    const rmSyncSpy = vi.spyOn(fs, 'rmSync').mockReturnValueOnce(void 0)
-    const cpSyncSpy = vi.spyOn(fs, 'cpSync').mockReturnValueOnce(void 0)
-    const result = await applyTranspilationResult(
-      { absolutePath: 'source_path', packageJson: { name: 'source' } },
-      { absolutePath: 'dest_path', packageJson: { name: 'dest' } },
-      'TRANSPILED_LEGACY',
-    )
-    expect(result).toEqual(true)
-    expect(rmSyncSpy.mock.lastCall).toEqual([
-      path.resolve('dest_path', 'node_modules/source/dist'),
-      { recursive: true },
-    ])
-    expect(cpSyncSpy.mock.lastCall).toEqual([
-      path.resolve('source_path', 'dist'),
-      path.resolve('dest_path', 'node_modules/source/dist'),
-      { recursive: true },
-    ])
+  it('should properly apply transpilation result for strategy: AMEND_NATIVE', async () => {
+
   })
 
-  it('should properly apply transpilation result for strategy: MAKEFILE_BUILD', async ({ expect }) => {
-    const rmSyncSpy = vi.spyOn(fs, 'rmSync').mockReturnValueOnce(void 0)
-    const cpSyncSpy = vi.spyOn(fs, 'cpSync').mockReturnValueOnce(void 0)
-    const result = await applyTranspilationResult(
-      { absolutePath: 'source_path', packageJson: { name: 'source' } },
-      { absolutePath: 'dest_path', packageJson: { name: 'dest' } },
-      'MAKEFILE_BUILD',
-    )
-    expect(result).toEqual(true)
-    expect(rmSyncSpy.mock.lastCall).toEqual([
-      path.resolve('dest_path', 'node_modules/source/dist'),
-      { recursive: true },
-    ])
-    expect(cpSyncSpy.mock.lastCall).toEqual([
-      path.resolve('source_path', 'dist'),
-      path.resolve('dest_path', 'node_modules/source/dist'),
-      { recursive: true },
-    ])
-  })
+  it('should properly apply transpilation result for strategy: NOBUILD_SOURCE', async () => {
 
-  it('should properly apply transpilation result for strategy: AMEND_NATIVE', async ({ expect }) => {
-    const rmSyncSpy = vi.spyOn(fs, 'rmSync').mockReturnValue(void 0)
-    const cpSyncSpy = vi.spyOn(fs, 'cpSync').mockReturnValue(void 0)
-    const result = await applyTranspilationResult(
-      { absolutePath: 'source_path', packageJson: { name: 'source' } },
-      { absolutePath: 'dest_path', packageJson: { name: 'dest' } },
-      'AMEND_NATIVE',
-    )
-    expect(result).toEqual(true)
-    expect(rmSyncSpy.mock.calls).toEqual([
-      [
-        path.resolve('dest_path', 'node_modules/source/amend'),
-        { recursive: true },
-      ],
-      [
-        path.resolve('dest_path', 'node_modules/source/boundaries'),
-        { recursive: true },
-      ],
-      [
-        path.resolve('dest_path', 'node_modules/source/lib'),
-        { recursive: true },
-      ],
-    ])
-    expect(cpSyncSpy.mock.calls).toEqual([
-      [
-        path.resolve('source_path', 'amend'),
-        path.resolve('dest_path', 'node_modules/source/amend'),
-        { recursive: true },
-      ],
-      [
-        path.resolve('source_path', 'boundaries'),
-        path.resolve('dest_path', 'node_modules/source/boundaries'),
-        { recursive: true },
-      ],
-      [
-        path.resolve('source_path', 'lib'),
-        path.resolve('dest_path', 'node_modules/source/lib'),
-        { recursive: true },
-      ],
-    ])
   })
-
-  it('should properly apply transpilation result for strategy: NOBUILD_SOURCE', async ({ expect }) => {
-    vi.spyOn(fs, 'existsSync').mockReturnValue(true)
-    const rmSyncSpy = vi.spyOn(fs, 'rmSync').mockReturnValue(void 0)
-    const cpSyncSpy = vi.spyOn(fs, 'cpSync').mockReturnValue(void 0)
-    const result = await applyTranspilationResult(
-      { absolutePath: 'source_path', packageJson: { name: 'source' } },
-      { absolutePath: 'dest_path', packageJson: { name: 'dest' } },
-      'NOBUILD_SOURCE',
-    )
-    expect(result).toEqual(true)
-    expect(rmSyncSpy.mock.calls).toEqual([
-      [
-        path.resolve('dest_path', 'node_modules/source/src'),
-        { recursive: true },
-      ],
-      [
-        path.resolve('dest_path', 'node_modules/source/lib'),
-        { recursive: true },
-      ],
-    ])
-    expect(cpSyncSpy.mock.calls).toEqual([
-      [
-        path.resolve('source_path', 'src'),
-        path.resolve('dest_path', 'node_modules/source/src'),
-        { recursive: true },
-      ],
-      [
-        path.resolve('source_path', 'lib'),
-        path.resolve('dest_path', 'node_modules/source/lib'),
-        { recursive: true },
-      ],
-    ])
-  })
-
 })
--- test/unit/core/get-workspace-root-path-for-package.test.ts
+++ test/unit/core/get-workspace-root-path-for-package.test.ts
@@ -1,80 +1,24 @@
-import { afterEach, describe, it, vi } from 'vitest'
-import * as GetChildPackagePathsForWorkspace from '../../../src/core/get-child-packages-paths-for-workspace'
-import { getWorkspaceRootPathForPackage } from '../../../src/core'
+import { describe, it } from 'vitest'
 
 describe.concurrent('getWorkspaceRootPathForPackage', () => {
-  afterEach(() => {
-    vi.clearAllMocks()
+
+  it('should return null if package does not belong to workspace', () => {
+
   })
 
-  it('should return null if package does not belong to workspace', ({ expect }) => {
-    vi.spyOn(GetChildPackagePathsForWorkspace, 'getChildPackagePathsForWorkspace')
-      // @ts-expect-error faulty type detection, because of overloads.
-      .mockReturnValue(null)
-    const result = getWorkspaceRootPathForPackage('some_package')
-    expect(result).toEqual(null)
+  it('should return null if package is within workspace folder structure, but not referenced by workspace root config', () => {
+
   })
 
-  it('should return null if package is within workspace folder structure, but not referenced by workspace root config', ({ expect }) => {
-    vi.spyOn(GetChildPackagePathsForWorkspace, 'getChildPackagePathsForWorkspace')
-      // @ts-expect-error faulty type detection, because of overloads.
-      .mockReturnValueOnce(null)
-      .mockReturnValue([ 'a', 'b', 'c' ])
-    const result = getWorkspaceRootPathForPackage('some_package')
-    expect(result).toEqual(null)
+  it('should return null when input is 10 levels down in depth', () => {
+
   })
 
-  it('should return null when input is 10 levels down in depth', ({ expect }) => {
-    vi.spyOn(GetChildPackagePathsForWorkspace, 'getChildPackagePathsForWorkspace')
-      // @ts-expect-error faulty type detection, because of overloads.
-      .mockReturnValueOnce(null)
-      // @ts-expect-error faulty type detection, because of overloads.
-      .mockReturnValueOnce(null)
-      // @ts-expect-error faulty type detection, because of overloads.
-      .mockReturnValueOnce(null)
-      // @ts-expect-error faulty type detection, because of overloads.
-      .mockReturnValueOnce(null)
-      // @ts-expect-error faulty type detection, because of overloads.
-      .mockReturnValueOnce(null)
-      // @ts-expect-error faulty type detection, because of overloads.
-      .mockReturnValueOnce(null)
-      // @ts-expect-error faulty type detection, because of overloads.
-      .mockReturnValueOnce(null)
-      // @ts-expect-error faulty type detection, because of overloads.
-      .mockReturnValueOnce(null)
-      // @ts-expect-error faulty type detection, because of overloads.
-      .mockReturnValueOnce(null)
-      // @ts-expect-error faulty type detection, because of overloads.
-      .mockReturnValueOnce(null)
-    const result = getWorkspaceRootPathForPackage('some_package')
-    expect(result).toEqual(null)
+  it('should return input path when input is root', () => {
+
   })
 
-  it('should return input path when input is root', ({ expect }) => {
-    vi.spyOn(GetChildPackagePathsForWorkspace, 'getChildPackagePathsForWorkspace')
-      .mockImplementationOnce((c) => {
-        if ((c as unknown as string) === 'root') {
-          return [ 'root/a', 'root/b', 'root/c' ]
-        }
-        return null as unknown as string[]
-      })
-    const result = getWorkspaceRootPathForPackage('root')
-    expect(result).toEqual('root')
-  })
+  it('should return paths when input is 3 levels down in depth', () => {
 
-  it('should return paths when input is 3 levels down in depth', ({ expect }) => {
-    vi.spyOn(GetChildPackagePathsForWorkspace, 'getChildPackagePathsForWorkspace')
-      // @ts-expect-error faulty type detection, because of overloads.
-      .mockReturnValueOnce(null)
-      // @ts-expect-error faulty type detection, because of overloads.
-      .mockReturnValueOnce(null)
-      .mockImplementationOnce((c) => {
-        if ((c as unknown as string) === 'root') {
-          return [ 'root/a', 'root/nested/some_package' ]
-        }
-        return null as unknown as string[]
-      })
-    const result = getWorkspaceRootPathForPackage('root/nested/some_package')
-    expect(result).toEqual('root')
   })
 })
